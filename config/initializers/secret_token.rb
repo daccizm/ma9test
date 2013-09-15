0333aba77acb50cff4f07234bbaf9abde67c0325
fukuishere <fukuishere@gmail.com>
2013-09-15 14:17:24 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ma9test::Application.config.secret_token = 'c6f89cad7dca6b71eb926e91804dde2b5970f09cf4df704fc87f20304ced02e3e1e2e9fdad905cef2b334d26386401e7a20dfe777135ec4870cd9ddc4163a98a'
